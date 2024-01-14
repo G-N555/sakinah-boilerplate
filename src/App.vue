791cd4805e7583fcde711b9db2ccfeed9706f565
gonakano <g.nakano555@gmail.com>
2024-01-14 20:39:44 +0900

    fix wording

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/about">About revert muslim</RouterLink>
       </nav>
     </div>
   </header>
